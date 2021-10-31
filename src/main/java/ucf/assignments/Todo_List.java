6401c2c09f1725125521b18da2c21d9195524019
MONIL PATEL <90346837+monilpatel1999@users.noreply.github.com>
2021-10-31 16:11:30 -0400

    Update Todo_List.java

--- src/main/java/ucf/assignments/Todo_List.java
+++ src/main/java/ucf/assignments/Todo_List.java
@@ -6,7 +6,7 @@ public class Todo_List {
     package ucf.assignments;
     /*
      *  UCF COP3330 Summer 2021 Assignment 4 Solution
-     *  Copyright 2021 Ayush Pindoria
+     *  Copyright 2021 Monil_Patel
      */
 
 import java.io.Serializable;
